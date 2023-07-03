30a44eac1502aebb1dd7ad0861114fa9a9a7b762
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-07-03 21:15:08 +0900

    Add files via upload

--- Class1/2675.py
+++ Class1/2675.py
@@ -1,15 +1,6 @@
-'''
 # 문자열 반복
 t = int(input())
-for i in range(t):
-    r, s = map(str, input().split())
-    r = int(r)
-    s = list(s)
-    for j in range(len(s)):
-        print(s[j]*r, end='')
-    print() # 줄넘김
-'''
-t = int(input())
+
 for i in range(t):
     r, s = map(str, input().split())
     for j in s:
