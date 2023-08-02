c8a87a5ff05c6bd79e7beae90f0b0814db62f1fb
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-08-03 02:10:39 +0900

    Add files via upload

--- Class2/18110.py
+++ Class2/18110.py
@@ -8,11 +8,8 @@ n = int(sys.stdin.readline())
 if n == 0:
     print(0)
 else:
-    ban = round2(n*0.15)
+    cut = round2(n*0.15)
     score = [int(sys.stdin.readline()) for _ in range(n)]
     score.sort()
-    rst = 0
-    for i in range(ban, n-ban): # sum()를 사용하지 않고 계산을 진행해야 한다!
-        rst += score[i]
-    rst = rst/(n-2*ban)
-    print(round2(rst))
+    rst = score[cut:n-cut] # 범위를 정확하게 잡아라!
+    print(round2(sum(rst)/len(rst)))
