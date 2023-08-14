7619e5bc960e00b4eec6901c65bbb247b39491ce
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-08-15 02:13:43 +0900

    Add files via upload

--- Class3/11723.py
+++ Class3/11723.py
@@ -8,24 +8,23 @@ S = set()
 for _ in range(m):
     x = sys.stdin.readline().split()
     if len(x) == 1:
-        command = x[0]
+        if 'all' in x:
+            S = set(range(1,21))
+        elif 'empty' in x:
+            S = set()
     else:
         command, target = x[0], int(x[1])
-    if target not in S and command == 'add':
-        S.add(target)
-    elif target in S and command == 'remove':
-        S.discard(target)
-    elif command == 'check':
-        if target in S:
-            print(1)
-        else:
-            print(0)
-    elif command == 'toggle':
-        if target in S:
-            S.discard(target)
-        else:
+        if command == 'add':
             S.add(target)
-    elif command == 'all':
-        S = set(range(1,21))
-    elif command == 'empty':
-        S = set()
+        elif command == 'remove':
+            S.discard(target)
+        elif command == 'check':
+            if target in S:
+                print(1)
+            else:
+                print(0)
+        elif command == 'toggle':
+            if target in S:
+                S.discard(target)
+            else:
+                S.add(target)
