b88c87fe84dd24eb25a30a4029cb327181ca6637
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-09-28 22:16:29 +0900

    Add files via upload

--- Class0/11021.py
+++ Class0/11021.py
@@ -1,6 +1,6 @@
-# A+B - 8
+# A+B - 7
 t = int(input())
 
 for i in range(t):
     a, b = map(int, input().split())
-    print(f"Case #{ i + 1 }: { a } + { b } = { a + b }")
+    print(f"Case #{ i + 1 }: { a + b }")
