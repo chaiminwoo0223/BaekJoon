3ceff3cd881e28083b1ed61469c39b570206cb42
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-12-27 16:04:55 +0900

    큐 2

---
+++ Silver4/B18258.py
@@ -0,0 +1,33 @@
+# 큐2
+from collections import deque
+import sys
+
+n = int(input())
+Deque = deque()
+
+for i in range(n):
+  cmd = sys.stdin.readline().split()
+  if cmd[0] == "push":
+    Deque.append(cmd[1])
+  elif cmd[0] == "pop":
+    if len(Deque) == 0:
+      print(-1)
+    else:
+      print(Deque.popleft())
+  elif cmd[0] == "size":
+    print(len(Deque))
+  elif cmd[0] == "empty":
+    if len(Deque) == 0:
+      print(-1)
+    else:
+      print(0)
+  elif cmd[0] == "front":
+    if len(Deque) == 0:
+      print(-1)
+    else:
+      print(Deque[0])
+  elif cmd[0] == "back":
+    if len(Deque) == 0:
+      print(-1)
+    else:
+      print(Deque[-1])
