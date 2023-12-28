3898aecf7cae61f4e1d7f7ba445fbe5437cc3c3f
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-12-28 18:59:48 +0900

    Standard

---
+++ Standard/10871.py
@@ -0,0 +1,7 @@
+# 정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+# list
+n, x = map(int, input().split())
+a = list(map(int, input().split()))
+for i in range(n):
+    if a[i] < x:
+        print(a[i], end=" ")
