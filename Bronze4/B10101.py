e09f1bc08726af63cc4e327c786cdfba4454f261
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2024-01-16 21:26:01 +0900

    삼각형 외우기

---
+++ Bronze4/B10101.py
@@ -0,0 +1,11 @@
+# 삼각형 외우기
+angles = [int(input()) for _ in range(3)]
+
+if sum(angles) != 180:
+    print("Error")
+elif len(set(angles)) == 1:
+    print("Equilateral")
+elif len(set(angles)) == 2:
+    print("Isosceles")
+else:
+    print("Scalene")
