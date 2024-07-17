0ed26b51ad25d4b29b51d1ea13eb0243ea95a89f
최민우 <chaiminwoo0223@naver.com>
2024-07-18 00:24:21 +0900

    서로 다른 부분 문자열의 개수

---
+++ Silver3/B11478.py
@@ -0,0 +1,11 @@
+# 서로 다른 부분 문자열의 개수
+import sys
+
+s = sys.stdin.readline().strip()
+result = set()
+
+for i in range(len(s)):
+    for j in range(i+1, len(s)+1):
+        result.add(s[i:j])
+
+print(len(result))
