103c781f822d55a66b9c48ccd433b59b6a61c4ab
Choi, Minwoo <chaiminwoo0223@gmail.com>
2024-11-12 00:33:37 +0900

    돌 게임

---
+++ Silver5/B9655.py
@@ -0,0 +1,17 @@
+# 돌 게임
+import sys
+
+n = int(sys.stdin.readline())
+count = 0
+
+while n != 0:
+    if n > 3:
+        n -= 3
+    else:
+        n -= 1
+    count += 1
+
+if count % 2 == 1:
+    print("SK")
+else:
+    print("CY")
