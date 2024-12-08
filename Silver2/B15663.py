af121d7214c3cb2477aeb2b8313cf188c2f712b6
Choi, Minwoo <chaiminwoo0223@gmail.com>
2024-12-08 18:58:54 +0900

    N과 M (9)

---
+++ Silver2/B15663.py
@@ -0,0 +1,9 @@
+# N과 M (9)
+from itertools import permutations
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+numbers = list(map(int, sys.stdin.readline().split()))
+
+for p in sorted(set(permutations(numbers, m))):
+    print(*p)
