bd7d232de76c4131164d5e0560fc07e4f597a76a
Choi, Minwoo <chaiminwoo0223@gmail.com>
2024-12-11 21:34:40 +0900

    N과 M (9)

--- Silver2/B15663.py
+++ Silver2/B15663.py
@@ -3,7 +3,7 @@ from itertools import permutations
 import sys
 
 n, m = map(int, sys.stdin.readline().split())
-numbers = list(map(int, sys.stdin.readline().split()))
+numbers = sorted(map(int, sys.stdin.readline().split()))
 
 for p in sorted(set(permutations(numbers, m))):
     print(*p)
