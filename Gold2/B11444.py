b1b8b1be9995b197cf1b35f1d0ecf8e0afbfe4dd
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-01-02 21:10:39 +0900

    피보나치 수 6

--- Gold2/B11444.py
+++ Gold2/B11444.py
@@ -3,20 +3,19 @@
 import sys
 
 n = int(sys.stdin.readline())
-mod = 1000000007
-number = {}
+number = {} # 핵심
 number[0] = 0
 number[1] = 1
 number[2] = 1
 
 def fibonacci(n):
-    k = n // 2
+    m = n // 2
     if n in number:
         return number[n]
-    elif n % 2 == 0: # n = 2k
-        number[n] = (fibonacci(k) * (fibonacci(k) + 2 * fibonacci(k-1))) % mod
-    else: # n = 2k + 1
-        number[n] = (fibonacci(k+1)**2 + fibonacci(k)**2) % mod
+    elif n % 2 == 0:
+        number[n] = (fibonacci(m) * (fibonacci(m) + 2 * fibonacci(m-1))) % 1000000007
+    else:
+        number[n] = (fibonacci(m+1)**2 + fibonacci(m)**2) % 1000000007
     return number[n]
 
 print(fibonacci(n))
--- README.md
+++ README.md
@@ -92,6 +92,6 @@
 - [B7795](https://www.acmicpc.net/problem/7795)
 
 ## 복습
-- [B19638](https://www.acmicpc.net/problem/19638) [X]
-- [B2749](https://www.acmicpc.net/problem/2749)
+- [B19638](https://www.acmicpc.net/problem/19638)
 - [B11444](https://www.acmicpc.net/problem/11444)
+- [B2749](https://www.acmicpc.net/problem/2749)
