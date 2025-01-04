db340d26fac99ac8f997cb0ee3d2e2cd0c6f68e9
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-01-04 23:56:24 +0900

    바이러스

--- Silver3/B2606.py
+++ Silver3/B2606.py
@@ -1,33 +1,28 @@
 # 바이러스
-# BFS
 from collections import deque
 import sys
 
-def bfs():
-    global count # 전역변수 수정 가능
-
-    while q:
-        current = q.popleft()
-        for next in range(1, n+1):
-            if not visited[next] and graph[current][next]:
-                visited[next] = True
-                q.append(next)
-                count += 1
-    return count
-
 n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
-graph = [[False] * (n+1) for _ in range(n+1)]
-visited = [False] * (n+1)
+graph = [[0] * (n+1) for _ in range(n+1)]
+visited = [0] * (n+1)
 q = deque()
-count = 0
 
-# 그래프 초기화
 for _ in range(m):
-    a, b = map(int, sys.stdin.readline().split())
-    graph[a][b] = True
-    graph[b][a] = True
+    x, y = map(int, sys.stdin.readline().split())
+    graph[x][y] = 1
+    graph[y][x] = 1
+
+def bfs():
+    while q:
+        x = q.popleft()
+        visited[x] = 1
+        for y in range(1, n+1):
+            if graph[x][y] == 1 and visited[y] == 0:
+                q.append(y)
 
 q.append(1)
-visited[1] = True
-print(bfs())
+visited[1] = 1
+bfs()
+
+print(sum(visited)-1)
