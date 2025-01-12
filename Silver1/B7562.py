e47f6a0b6e083c0d3393be5aa19fb3aebcee7d16
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-01-12 23:26:38 +0900

    나이트의 이동

--- Silver1/B7562.py
+++ Silver1/B7562.py
@@ -1,33 +1,37 @@
 # 나이트의 이동
-# 누적합
 from collections import deque
 import sys
 
-def bfs(e, f, g, h, i):
-    chess = [[0] * i for _ in range(i)]
-    q = deque()
-    q.append([e, f])
-    chess[e][f] = 1 # 핵심: 방문 표시
-
-    while q:
-        x, y = q.popleft()
-        if x == g and y == h:
-            return chess[x][y] - 1 # 핵심2: 시작 지점 제외
-        
-        for j in range(8):
-            tx = x + dx[j]
-            ty = y + dy[j]
-            if 0 <= tx < i and 0 <= ty < i and chess[tx][ty] == 0:
-                chess[tx][ty] = chess[x][y] + 1 # 핵심3: 이동 횟수 갱신
-                q.append([tx, ty])
-    return -1
-
 t = int(sys.stdin.readline())
-dx = [-2, -2, -1, 1, 2, 2, 1, -1]
-dy = [-1, 1, 2, 2, 1, -1, -2, -2]
+dx = [2, 2, 1, 1, -1, -1, -2, -2]
+dy = [1, -1, 2, -2, 2, -2, 1, -1]
+q = deque()
 
+def bfs():
+    result = []
+    while q:
+        x, y, count = q.popleft()
+
+        if x == u and y == v:
+            result.append(count)
+            graph[x][y] = 0
+            
+        for i in range(8):
+            tx = x + dx[i]
+            ty = y + dy[i]
+
+            if 0 <= tx < n and 0 <= ty < n and graph[tx][ty] == 0:
+                q.append([tx, ty, count+1])
+                graph[tx][ty] = 1
+
+    return min(result)
+                
 for _ in range(t):
-    i = int(sys.stdin.readline())
-    a, b = map(int, sys.stdin.readline().split())
-    c, d = map(int, sys.stdin.readline().split())
-    print(bfs(a, b, c, d, i))
+    n = int(sys.stdin.readline())
+    graph = [[0] * n for _ in range(n)]
+    x, y = map(int, sys.stdin.readline().split())
+    graph[x][y] = 1
+    u, v = map(int, sys.stdin.readline().split())
+    q.append([x, y, 0])
+
+    print(bfs())
