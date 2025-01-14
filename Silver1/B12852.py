22d72c6cea26dcb1ecd24665c15819e52fe518c0
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-01-14 16:23:26 +0900

    1로 만들기 2

--- Silver1/B12852.py
+++ Silver1/B12852.py
@@ -1,30 +1,33 @@
 # 1로 만들기 2
+# 다이나믹 프로그래밍
+# 최솟값
 from collections import deque
 import sys
 
 n = int(sys.stdin.readline())
+visited = [0] * (n+1) # 핵심
+q = deque()
 
-def bfs(x):
-    visited = [0] * (n+1) # n + 1 크기의 방문 체크 배열 생성
-    q = deque([[x, []]]) # 핵심: 큐에 초깃값과 연산 경로 추가
-    visited[x] = 1 # 핵심2: 초깃값 방문 체크
-
+def bfs():
     while q:
-        x, path = q.popleft()
-        path = path + [x] # 핵심3: 현재 값을 경로에 추가
+        x, numbers = q.popleft()
+        numbers = numbers + [x] # 핵심
 
-        if x == 1: # 1에 도달하면 연산 횟수와 경로 반환
-            return len(path) - 1, path
+        if x == 1:
+            return len(numbers) - 1, numbers
         if x % 3 == 0 and visited[x // 3] == 0:
             visited[x // 3] = 1
-            q.append([x // 3, path])
+            q.append([x // 3, numbers])
         if x % 2 == 0 and visited[x // 2] == 0:
             visited[x // 2] = 1
-            q.append([x // 2, path]) 
+            q.append([x // 2, numbers])
         if x - 1 > 0 and visited[x - 1] == 0:
             visited[x - 1] = 1
-            q.append([x - 1, path])
-    
-step, path = bfs(n)
+            q.append([x - 1, numbers])
+
+q.append([n, []])
+visited[n] = 1
+step, numbers = bfs()
+
 print(step)
-print(*path)
+print(*numbers)
