d73c6a4b092eba1a868068056f6f2efb488ecf0a
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-01-14 14:07:17 +0900

    트리 순회

--- Silver1/B1991.py
+++ Silver1/B1991.py
@@ -1,35 +1,36 @@
 # 트리 순회
 # Python으로 트리를 쉽게 구현하는 방법: Dictionary 사용
+# map을 사용하지 않아도, 공백을 기준으로 값을 나눌 수 있다.
 import sys
 
 n = int(sys.stdin.readline())
 tree = dict()
 
-for i in range(n):
-    root, left, right = map(str, sys.stdin.readline().split())
+for _ in range(n):
+    root, left, right = sys.stdin.readline().split()
     tree[root] = [left, right]
 
-# 전위 순회: 루트 -> 왼쪽 자식 -> 오른쪽 자식
+# 전위 순회
 def preorder(root):
     if root != '.':
-        print(root, end='') # 루트
-        preorder(tree[root][0]) # 왼쪽 자식
-        preorder(tree[root][1]) # 오른쪽 자식
+        print(root, end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
 
-# 중위 순회: 왼쪽 자식 -> 루트 -> 오른쪽 자식
+# 중위 순회
 def inorder(root):
     if root != '.':
-        inorder(tree[root][0]) # 왼쪽 자식
-        print(root, end='') # 루트
-        inorder(tree[root][1]) # 오른쪽 자식
+        inorder(tree[root][0])
+        print(root, end='')
+        inorder(tree[root][1])
 
-# 후위 순회: 왼쪽 자식 -> 오른쪽 자식 -> 루트
+# 후위 순회
 def postorder(root):
     if root != '.':
-        postorder(tree[root][0]) # 왼쪽 자식
-        postorder(tree[root][1]) # 오른쪽 자식
-        print(root, end='') # 루트
-        
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end='')
+
 preorder('A')
 print()
 inorder('A')
