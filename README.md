c3df335fee0a0c6566ca763d77d99091a3cd4045
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-01-22 21:49:57 +0900

    상근이의 여행

--- README.md
+++ README.md
@@ -75,9 +75,9 @@
 - [B7795](https://www.acmicpc.net/problem/7795)
 
 ## 복습
-- [B9372](https://www.acmicpc.net/problem/9372) [X]
 - [B2630](https://www.acmicpc.net/problem/2630)
 - [B12931](https://www.acmicpc.net/problem/12931) [G]
 - [B1697](https://www.acmicpc.net/problem/1697)
 - [B1057](https://www.acmicpc.net/problem/1057)
 - [B12852](https://www.acmicpc.net/problem/12852) [X]
+- [B9372](https://www.acmicpc.net/problem/9372) [X]
--- Silver4/B9372.py
+++ Silver4/B9372.py
@@ -1,29 +1,30 @@
 # 상근이의 여행
+from collections import Counter
 import sys
 
 def dfs(v):
     global count
+
     visited[v] = 1
     count += 1
+    
     for next in range(1, n+1):
         if visited[next] == 0 and graph[v][next] == 1:
             dfs(next)
 
-t = int(sys.stdin.readline())
-
-for _ in range(t):
+for _ in range(int(sys.stdin.readline())):
     n, m = map(int, sys.stdin.readline().split())
     graph = [[0] * (n+1) for _ in range(n+1)]
     visited = [0] * (n+1)
-    number = set()
+    counter = Counter()
     count = 0
 
     for _ in range(m):
         a, b = map(int, sys.stdin.readline().split())
         graph[a][b] = 1
         graph[b][a] = 1
-        number.add(a)
-        number.add(b)
+        counter[a] += 1
+        counter[b] += 1
 
-    dfs(min(number))
+    dfs(counter.most_common()[-1][0])
     print(count - 1)
