16c365f0fdc7b3c244293bbc849a2267adc8960d
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-01-25 00:24:32 +0900

    알고리즘 수업 - 깊이 우선 탐색 2

---
+++ Silver2/B24480.py
@@ -0,0 +1,28 @@
+# 알고리즘 수업 - 깊이 우선 탐색 2
+import sys
+sys.setrecursionlimit(200000)
+
+n, m, r = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+count = 1
+
+for _ in range(m):
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+def dfs(x):
+    global count
+
+    for next in sorted(graph[x], reverse=True):
+        if visited[next] == 0:
+            count += 1
+            visited[next] = count
+            dfs(next)
+
+visited[r] = 1
+dfs(r)
+
+for i in range(1, n+1):
+    print(visited[i])
