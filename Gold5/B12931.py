0b428bca76cab6242980f645306930dd74e37c4e
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-01-24 16:56:27 +0900

    두 배 더하기

--- Gold5/B12931.py
+++ Gold5/B12931.py
@@ -6,12 +6,13 @@ b = list(map(int, sys.stdin.readline().split()))
 count = 0
 
 while True:
+    # 짝수 만들기
     for i in range(n):
         if b[i] % 2 == 1:
-            b[i] -= 1 # 핵심
+            b[i] -= 1
             count += 1
 
-    if sum(b) == 0:
+    if max(b) == 0:
         print(count)
         break
     else:
