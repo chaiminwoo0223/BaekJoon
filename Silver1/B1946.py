70f791223266b5fb84c01f7b68a5fc84ebd5d425
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-01-25 17:05:41 +0900

    신입 사원

--- Silver1/B1946.py
+++ Silver1/B1946.py
@@ -1,16 +1,16 @@
 # 신입 사원
+# float('inf')
 import sys
 
 for _ in range(int(sys.stdin.readline())):
     n = int(sys.stdin.readline())
-    numbers = sorted(list(map(int, sys.stdin.readline().split())) for _ in range(n))
+    ranking = sorted([tuple(map(int, sys.stdin.readline().split())) for _ in range(n)]) # 핵심
     count = 0
-    interview = n
+    interview = float('inf')
     
-    for x, y in numbers:
+    for _, y in ranking:
         if y < interview: # 핵심
             count += 1
             interview = y
-            print(x, y, count)
         
     print(count)
