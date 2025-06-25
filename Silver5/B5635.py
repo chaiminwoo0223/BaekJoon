066d94530eaf2e8e7e826cf72331609437b2c875
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-06-25 21:41:53 +0900

    생일

--- Silver5/B5635.py
+++ Silver5/B5635.py
@@ -1,14 +1,9 @@
-# 생일
-# students.append([name, int(day), int(month), int(year)]) # 리스트 추가
-# students.sort(key=lambda x: (x[3], x[2], x[1])) # 년, 월, 일 순으로 정렬
-n = int(input())
 students = []
 
-for i in range(n):
+for i in range(int(input())):
     name, day, month, year = input().split()
-    students.append([int(year), int(month), int(day), name]) # [년, 월, 일, 이름]
+    students.append([int(year), int(month), int(day), name])
 
-students.sort() # 오름차순 정렬
-
-print(students[-1][3]) # 가장 나이가 적은 사람
-print(students[0][3]) # 가장 나이가 많은 사람
+students.sort()
+print(students[-1][-1]) # 가장 나이가 적은 사람
+print(students[0][-1]) # 가장 나이가 많은 사람
