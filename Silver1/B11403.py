1467cbb137da36cef91227fc220a83f40cdcca47
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-06-28 01:30:03 +0900

    경로 찾기

--- Silver1/B11403.py
+++ Silver1/B11403.py
@@ -1,24 +1,24 @@
 # 경로 찾기
-# 플로이드 워샬
 # 한 정점에서 갈 수 있는 모든 정점을 검토
-import sys
+n = int(input())
 
-n = int(sys.stdin.readline())
-graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+graph = [list(map(int, input().split())) for _ in range(n)]
 visited = [0] * n
 
 def dfs(x):
-    for i in range(n):
-        if graph[x][i] == 1 and visited[i] == 0:
-            visited[i] = 1
-            dfs(i)
+    for j in range(n):
+        if graph[x][j] == 1 and visited[j] == 0:
+            visited[j] = 1
+            dfs(j)
 
 for i in range(n):
-    dfs(i)
     for j in range(n):
-        if visited[j] == 1:
-            print(1, end=" ")
-        else:
-            print(0, end=" ")
+        if graph[i][j] == 1 and visited[j] == 0:
+            visited[j] = 1
+            dfs(j)
+
+    for k in range(n):
+        print(visited[k], end=" ")
     print()
+
     visited = [0] * n # 초기화
