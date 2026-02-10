bc3396d3b8b596d0977fa1eb78591945775548de
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-02-10 20:55:21 +0900

    두 배 더하기

--- Gold5/B12931.py
+++ Gold5/B12931.py
@@ -1,21 +1,22 @@
 # 두 배 더하기
-import sys
-
-n = int(sys.stdin.readline())
-b = list(map(int, sys.stdin.readline().split()))
-count = 0
+n = int(input())
+a = [0] * n
+b = list(map(int, input().split()))
+cnt = 0
 
 while True:
-    # 짝수 만들기
+    # 더하기 연산
     for i in range(n):
         if b[i] % 2 == 1:
             b[i] -= 1
-            count += 1
+            cnt += 1
 
     if max(b) == 0:
-        print(count)
+        print(cnt)
         break
-    else:
-        for i in range(n):
-            b[i] //= 2
-        count += 1
+
+    # 곱하기 연산
+    for i in range(n):
+        b[i] //= 2
+
+    cnt += 1
