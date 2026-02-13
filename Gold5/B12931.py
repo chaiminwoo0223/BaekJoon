b981f0927462aaad83f4c7f06bab55a9b5595248
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-02-13 22:15:11 +0900

    두 배 더하기

--- Gold5/B12931.py
+++ Gold5/B12931.py
@@ -1,11 +1,10 @@
 # 두 배 더하기
 n = int(input())
-a = [0] * n
 b = list(map(int, input().split()))
 cnt = 0
 
 while True:
-    # 더하기 연산
+    # 1) 배열에 있는 값 하나를 1 증가시킨다.
     for i in range(n):
         if b[i] % 2 == 1:
             b[i] -= 1
@@ -15,7 +14,7 @@ while True:
         print(cnt)
         break
 
-    # 곱하기 연산
+    # 2) 배열에 있는 모든 값을 두 배 시킨다.
     for i in range(n):
         b[i] //= 2
 
