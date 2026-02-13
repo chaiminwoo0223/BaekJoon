a41289aaf94c2f970dcb81a21094abc3e029b0c8
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-02-13 22:04:41 +0900

    예산

--- Silver2/B2512.py
+++ Silver2/B2512.py
@@ -2,23 +2,21 @@
 n = int(input())
 pays = sorted(map(int, input().split()))
 m = int(input())
-start, end = 0, pays[-1] # 핵심
+start, end = 0, pays[-1]
 
 while start <= end:
     mid = (start + end) // 2
-    t = 0
+    result = 0
 
     for pay in pays:
         if pay < mid:
-            t += pay
+            result += pay
         else:
-            t += mid
+            result += mid
 
-    if t <= m:
+    if result <= m:
         start = mid + 1
     else:
         end = mid - 1
-
-    print(start, end, mid)
 
 print(end)
