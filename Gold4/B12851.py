7d553d7d6dfd3919df8be154920e1dce784a0fbe
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-02-13 20:12:53 +0900

    숨바꼭질 2

--- Gold4/B12851.py
+++ Gold4/B12851.py
@@ -1,29 +1,28 @@
 # 숨바꼭질 2
+# 반례: 시작지와 목적지가 같은 경우 0이 되어야 한다.
 from collections import deque
-import sys
 
-n, k = map(int, sys.stdin.readline().split())
+n, k = map(int, input().split())
 visited = [-1] * 100001
-q = deque([(n)])
+q = deque([n])
 visited[n] = 0
-count = 0
+cnt = 0
 
 def bfs():
-    global count
+    global cnt
 
     while q:
         x = q.popleft()
 
         if x == k:
-            count += 1
-            
-        for next in (x-1, x+1, 2*x):
-            if 0 <= next < 100001:
-                if visited[next] == -1 or visited[next] == visited[x] + 1: # 핵심
-                    q.append(next)
-                    visited[next] = visited[x] + 1
+            cnt += 1
+
+        for next in (x-1, x+1, x*2):
+            if 0 <= next < 100001 and (visited[next] == -1 or visited[next] == visited[x] + 1): # 핵심
+                q.append(next)
+                visited[next] = visited[x] + 1
 
 bfs()
 
 print(visited[k])
-print(count)
+print(cnt)
