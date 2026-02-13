388bc2fb52d0dd9a6c230b267e2d4ca566ba50a8
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-02-13 21:19:19 +0900

    먹을 것인가 먹힐 것인가

--- Silver3/B7795.py
+++ Silver3/B7795.py
@@ -1,21 +1,20 @@
 # 먹을 것인가 먹힐 것인가
 # 이분탐색
-t = int(input())
-
-for _ in range(t):
+# 인덱스
+for _ in range(int(input())):
     n, m = map(int, input().split())
     a = sorted(map(int, input().split()))
     b = sorted(map(int, input().split()))
     cnt = 0
 
-    for i in range(n):
+    for num in a:
         start = 0
         end = m - 1
 
         while start <= end:
             mid = (start + end) // 2
 
-            if a[i] > b[mid]:
+            if num > b[mid]:
                 start = mid + 1
             else:
                 end = mid - 1
