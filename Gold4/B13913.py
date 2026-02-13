00306ff6750ac13875992ba08cee125875ff2239
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-02-13 19:25:09 +0900

    숨바꼭질 4

--- Gold4/B13913.py
+++ Gold4/B13913.py
@@ -1,21 +1,10 @@
 # 숨바꼭질 4
 from collections import deque
-import sys
 
-n, k = map(int, sys.stdin.readline().split())
+n, k = map(int, input().split())
 visited = [0] * 100001
 moved = [0] * 100001
-q = deque()
-
-def pathes(x):
-    result = []
-    temp = x
-
-    for _ in range(visited[x]+1):
-        result.append(temp)
-        temp = moved[temp]
-        
-    print(*result[::-1])
+q = deque([n])
 
 def bfs():
     while q:
@@ -23,14 +12,23 @@ def bfs():
 
         if x == k:
             print(visited[x])
-            pathes(x)
-            return
-        
-        for next in (x-1, x+1, 2*x):
-            if 0 <= next < 100001 and visited[next] == 0:
+            path(x)
+            break
+
+        for next in (x-1, x+1, x*2):
+            if 0 <= next < 100001 and not visited[next]:
                 q.append(next)
                 visited[next] = visited[x] + 1
                 moved[next] = x
 
-q.append(n)
+def path(x):
+    result = []
+    t = x
+
+    for _ in range(visited[x]+1):
+        result.append(t)
+        t = moved[t]
+
+    print(*result[::-1])
+
 bfs()
