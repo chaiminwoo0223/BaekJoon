ddcfed1df20aa6343b1c76b315c9f745ebdf672f
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-02-20 23:39:05 +0900

    색종이 만들기

--- Silver2/B2630.py
+++ Silver2/B2630.py
@@ -1,25 +1,26 @@
 # 색종이 만들기
+# DFS
 # 분할 정복 : 재귀적으로 자신을 호출하면서, 그 연산의 단위를 조금씩 줄여가는 방식
-import sys
-
-n = int(sys.stdin.readline())
-graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-numbers = [0, 0]
+# 재귀
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+result = [0, 0] # 파란색, 흰색
 
 def dfs(x, y, n):
-    color = graph[x][y] # 핵심
+    paper = graph[x][y]
 
     for i in range(x, x+n):
         for j in range(y, y+n):
-            if color != graph[i][j]:
+            if paper != graph[i][j]: # 핵심
                 dfs(x, y, n // 2)
-                dfs(x, y + n // 2, n // 2)
                 dfs(x + n // 2, y, n // 2)
+                dfs(x, y + n // 2, n // 2)
                 dfs(x + n // 2, y + n // 2, n // 2)
                 return
-    
-    numbers[color] += 1
+
+    result[paper] += 1
 
 dfs(0, 0, n)
-print(numbers[0])
-print(numbers[1])
+
+print(result[0])
+print(result[1])
