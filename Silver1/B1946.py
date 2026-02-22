d604225df40eeabcaedbb3b1bbe4f4fc604ba490
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-02-23 01:20:01 +0900

    신입 사원

--- Silver1/B1946.py
+++ Silver1/B1946.py
@@ -1,16 +1,24 @@
 # 신입 사원
-# float('inf')
+# 성적의 순위
+# 그리디 알고리즘
+# 정렬
 import sys
 
 for _ in range(int(sys.stdin.readline())):
     n = int(sys.stdin.readline())
-    ranking = sorted([tuple(map(int, sys.stdin.readline().split())) for _ in range(n)]) # 핵심
-    count = 0
-    interview = float('inf')
-    
-    for _, y in ranking:
-        if y < interview: # 핵심
-            count += 1
-            interview = y
-        
-    print(count)
+    result = []
+
+    for _ in range(n):
+        a, b = map(int, sys.stdin.readline().split())
+        result.append([a, b])
+
+    result = sorted(result, key=lambda x: x[0]) # 핵심
+    cnt = 1 # 무조건 1명은 뽑는다.
+    t = result[0][1]
+
+    for i in range(1, n):
+        if result[i][1] < t:
+            t = result[i][1]
+            cnt += 1
+
+    print(cnt)
