729f011e7eb4bef4f04546a659d6834c212d2785
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-02-25 00:02:52 +0900

    신입 사원

--- Silver1/B1946.py
+++ Silver1/B1946.py
@@ -1,24 +1,25 @@
 # 신입 사원
-# 성적의 순위
-# 그리디 알고리즘
-# 정렬
+# 그리디 알고리즘, 정렬
+# 순위
 import sys
 
-for _ in range(int(sys.stdin.readline())):
-    n = int(sys.stdin.readline())
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n = int(input())
     result = []
 
     for _ in range(n):
-        a, b = map(int, sys.stdin.readline().split())
+        a, b = map(int, input().split())
         result.append([a, b])
 
-    result = sorted(result, key=lambda x: x[0]) # 핵심
-    cnt = 1 # 무조건 1명은 뽑는다.
-    t = result[0][1]
+    result = sorted(result, key=lambda x: x[0]) # 서류심사 성적
+    cnt = 1
+    x = result[0][1]
 
-    for i in range(1, n):
-        if result[i][1] < t:
-            t = result[i][1]
+    for i in range(1, n): # 면접 성적
+        if x > result[i][1]:
             cnt += 1
+            x = result[i][1]
 
     print(cnt)
