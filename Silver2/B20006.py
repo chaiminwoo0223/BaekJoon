f0eaa198353d6c83fe8c5226b113123099e08784
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-03-23 15:15:54 +0900

    랭킹전 대기열

---
+++ Silver2/B20006.py
@@ -0,0 +1,33 @@
+# 랭킹전 대기열
+import sys
+
+input = sys.stdin.readline
+
+p, m = map(int, input().split())
+rooms = []
+
+for _ in range(p):
+    l, n = input().split()
+    l = int(l)
+
+    if not rooms:
+        rooms.append([(l, n)])
+        continue
+
+    for room in rooms:
+        if len(room) == m:
+            continue
+        elif room[0][0] - 10 <= l <= room[0][0] + 10:
+            room.append((l, n))
+            break
+    else:
+        rooms.append([(l, n)])
+
+for room in rooms:
+    if len(room) == m:
+        print("Started!")
+    else:
+        print("Waiting!")
+
+    for level, name in sorted(room, key = lambda x : x[1]):
+        print(level, name)
